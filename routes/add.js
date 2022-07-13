7f62bb3994806c408b391b85f8184625de48fe20
Caleb Kim <kicale@amazon.com>
2022-07-12 17:33:33 -0700

    Update jade to pug

--- routes/add.js
+++ routes/add.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* Add a new room */
-router.get('/', function(req, res, next) {
-    res.render('add', { title: 'Add new room' });
-});
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 module.exports = router;
