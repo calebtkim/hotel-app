22eeb27298317bb9632c8a7f7d4d241220d04c27
Caleb Kim <kicale@amazon.com>
2022-07-13 03:57:45 +0000

    Update routes

--- routes/create.js
+++ routes/create.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', function(req, res, next) {
   console.log('create');
-  res.render('index', { title: 'AP Hotel', message: 'Create room!' })
+  res.render('index', { title: 'AP Hotel', message: 'Create room!' });
+  res.send('Create completed.');
 })
 
 module.exports = router;
