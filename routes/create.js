bc82dce333502734dc4943c4bc98f7a447804400
Caleb Kim <kicale@amazon.com>
2022-07-13 03:09:17 +0000

    Add missing xray auto-instrumentation file

--- routes/create.js
+++ routes/create.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log('create');
   res.render('index', { title: 'AP Hotel', message: 'Create room!' })
 })
 
--- routes/rooms.js
+++ routes/rooms.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* display room list */
 router.get('/', (req, res) => {
+  console.log('list');
   res.render('index', { title: 'AP Hotel', message: 'List rooms!' })
 })
 
